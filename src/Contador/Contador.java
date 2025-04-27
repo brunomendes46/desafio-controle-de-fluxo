f019665843c765cab8b0fbb2b2015816c0807008
Bruno Mendes <bruno.mendess@hotmail.com>
2025-04-27 00:38:50 -0300

    resultado final

---
+++ src/Contador/Contador.java
@@ -0,0 +1,46 @@
+package Contador;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            try {
+                contar(parametroUm, parametroDois);
+            } catch (ParametrosInvalidosException exception) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
+
+            System.out.println("Deseja realizar uma nova contagem? (s/n)");
+            String resposta = terminal.next();
+
+            if (resposta.equalsIgnoreCase("n")) {
+                continuar = false;
+                System.out.println("Programa encerrado.");
+            }
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
